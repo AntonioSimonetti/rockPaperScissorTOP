a97247b5ba330d2fe5a2b38b87883060798b6602
Antonio Simonetti <97043521+AntonioSimonetti@users.noreply.github.com>
2022-04-12 10:01:37 +0200

    Add files via upload
    Ended the project

--- javascript.js
+++ javascript.js
@@ -1,55 +1,65 @@
-const movePC = ["Rock", "Paper","Scissors"];
+const movePC = ["rock", "paper","scissors"];
+
 
 function computerPlay() {
-
-   return movePC[Math.floor(Math.random() * movePC.length)];
-};
-
-function playRound( ) {
-
-  let playerSelection = playerPlay();
-  let computerSelection = computerPlay();
-
-   if(playerSelection == computerSelection){
-      return "tie";
-  } else if (
-      (playerSelection === "rock" && computerSelection == "scissors") ||
-      (playerSelection === "paper" && computerSelection == "rock") || 
-      (playerSelection === "scissor" && computerSelection == "paper") 
-      ) {
-      return "Player";
-  }   else{
-      return "Computer";
-  }
+   
+   return movePC[Math.floor(Math.random() * movePC.length)]
 };
 
 function playerPlay() {
-   let playerChoose = prompt("Choose rock, paper or scissor!");
+   let playerChoose = prompt("Choose rock, paper or scissors!");
+   
     if (playerChoose) {
        playerChoose = playerChoose.toLowerCase();
-       if(playerChoose === "rock" || playerChoose === "paper" || playerChoose === "scissor") {
+       if(playerChoose === "rock" || playerChoose === "paper" || playerChoose === "scissors") {
           return playerChoose;
-       } else {
-          return prompt("You should choose between rock, paper or scissor!");
-       }
-    }  
+       } else { return "retry" } 
+    } 
+   return "retry";
 };
 
+function playRound() {
+
+  let playerSelection = playerPlay();
+  let computerSelection = computerPlay();
+   if (playerSelection === computerSelection) {
+      return "Tie"; }
+   else if (playerSelection === "rock" && computerSelection === "scissors" ||
+    playerSelection === "scissors" && computerSelection === "paper"  || 
+    playerSelection === "paper" && computerSelection == "rock") 
+     {
+   return "Player";
+}   else{
+   return "Computer";
+}
+};
 
 function game () {
    let winnerPlayer = 0;
    let winnerPc = 0;
    for (let i = 0; i < 5; i++) {
-      if (playRound === "Player") { 
-         winnerPlayer ++;
-      } else if (playRound === "Computer") {
-         winnerPc ++;
-      }
-   if (winnerPlayer === 5) {or
-      return "Computer win the game!"
-   }  else { 
-      return "Play another round!"
-   }
 
-   }
+      const roundPo = playRound();
+      if (roundPo === "Player") { 
+         winnerPlayer ++;
+         console.log(winnerPlayer);
+        
+      } else if (roundPo === "Computer") {
+         winnerPc ++;
+         console.log(winnerPc);
+         
+      }
+  
+  }
+  if (winnerPlayer > winnerPc) {
+   console.log("You win the game!")
+}  else if (winnerPc > winnerPlayer) { 
+   console.log("Computer win the game")
+} else { 
+   console.log("Play another round!")
+}  
 }; 
+
+game();
+
+
